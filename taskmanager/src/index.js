0a793e9280d9eac16490d4b3921dfe82cf2433da
Дмитрий Багаев <sequensor@mail.ru>
2019-03-23 20:21:33 +0600

    fix pages

--- taskmanager/src/index.js
+++ taskmanager/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Base from './layouts/base/Base';
-import toDo from './pages/toDo/toDo';
+import ToDo from './pages/todo/ToDo';
+import Done from './pages/done/Done';
 
 ReactDOM.render(
-    <Base><toDo/></Base>, document.getElementById('root')
+    //<Base><Done /></Base>, document.getElementById('root')
+    <Base><ToDo /></Base>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
