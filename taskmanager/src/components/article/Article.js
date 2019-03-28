467187ccd4d2ebd9ff6cc677f353dbd51295ff63
Дмитрий Багаев <sequensor@mail.ru>
2019-03-28 14:28:00 +0600

    add icons

--- taskmanager/src/components/article/Article.js
+++ taskmanager/src/components/article/Article.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
+
 export default class Article extends React.Component {
     render() {
         return (
             <article className="article">
+                <a href="#" className="article__link">
+                    <div className="article__checkbox article__icon"></div>
+                </a>
                 <p className="article__description">{this.props.description}</p>
+                <a href="#" className="article__link">
+                    <div className="article__pencil article__icon"></div>
+                </a>
+                <a href="#" className="article__link">
+                    <div className="article__trash article__icon"></div>
+                </a>
             </article>
         );
     };
